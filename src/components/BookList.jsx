1fd09475cbd0ea8e64f8444e355e64a8aeeb76b8
hailia sommerville <hailiassommerville@gmail.com>
2025-06-25 22:20:21 -0400

    Card display added

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,39 +3,8 @@ import BookCard
  from "./BookCard";
 const BookList = ({books}) => {
 
-  return (
-    <>
-      {/*{books.map((book) => (
-    <li key={book.id}>{book.title} by {book.author} {book.rating} stars,
-     URL: {book.image}, Pub: {book.published}, Decrip: {book.description}, 
-     Cat: {book.category}, Read: {book.isRead}</li>
-  ))}
-  */}
-
-  {/*<table>
-  <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Category</th>
-          <th>Description</th>
-          <th>Published</th>
-          <th>Rating</th>
-        </tr>
-      </thead>
-    {books.map((book) => (
-    <tr>
-      <td key={book.id}>{book.title}</td>
-      <td key={book.id}>{book.author}</td>
-      <td key={book.id}>{book.category}</td>
-      <td key={book.id}>{book.description}</td>
-      <td key={book.id}>{book.published}</td>
-      <td key={book.id}>{book.rating}</td>
-      <td key={book.id}>{book.isRead}</td>
-      <td key={book.id}>{book.isFav}</td>
-      </tr>
-    ))}
-  </table> */}
+  return ( 
+  <>
    <BookCard books={books}/>
   </>
   );
